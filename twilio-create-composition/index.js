e2f0a432163ca21a4a4fe2c2449e25470b358d5f
Akshay C. Gollapalli <akshay.gollapalli@gmail.com>
2023-07-10 15:06:44 -0500

    support retry logicc

--- twilio-create-composition/index.js
+++ twilio-create-composition/index.js
@@ -1,9 +1,13 @@
 import {default as twilio} from 'twilio';
 
 const handleSuccess = (data) => {
+  console.log(data);
   return {
     statusCode: 200,
-    body: JSON.stringify(data.sid),
+    body: JSON.stringify({
+      status: 'completed',
+      compositionJob: data.sid
+    }),
   };
 }
 
@@ -11,38 +15,86 @@ const handleError = (error) => {
   console.error(error);
   return {
     statusCode: 504,
-    body: JSON.stringify(error)
+    body: JSON.stringify({
+      status: 'error',
+      body: error
+    })
   }
 }
 
 export const handler = async(event) => {
 
   console.log(event);
-
-  // retrieved from event
-  const roomSid = event.roomSid;
-
   const accountSid = process.env.TWILIO_ACCOUNT_SID;
   const authToken = process.env.TWILIO_AUTH_TOKEN;
   const statusCallback = process.env.STATUS_CALLBACK_URI;
   const client = twilio(accountSid, authToken);
 
-  const response = await client.video.v1.compositions
-                         .create({
-                           videoLayout: {
-                             transcode: {
-                               video_sources: [
-                                 '*'
-                               ]
-                             }
-                           },
-                           statusCallback: statusCallback,
-                           format: 'mp4',
-                           roomSid: roomSid,
-                           xTwilioWebhookEnabled: true
-                         })
-                         .then(handleSuccess)
-                         .catch(handleError);
-  return response;
-};
 
+  // retrieved from event
+  const roomSid = event.roomSid;
+  const uniqueName = event.uniqueName;
+
+  console.log(roomSid ? roomSid : uniqueName)
+
+  let room;
+  if (roomSid)
+    {
+      room = await client.video.v1.rooms(roomSid)
+                           .fetch()
+                           .catch(handleError);
+    }
+  else {
+    const rooms = await client.video.v1//.rooms(encodeURIComponent(roomSid ? roomSid : uniqueName))
+                           rooms.list({
+                             uniqueName: uniqueName
+                           })
+                           .catch(handleError);
+    if (rooms.length > 0) {
+      room = rooms[0];
+    }
+  }
+
+  if (!room){
+    return handleError('could not find room')
+  }
+  else if (room?.statusCode) {
+    return room;
+  }
+  else if (room.sid === 'failed') {
+    return {
+      statusCode: 504,
+      body: JSON.stringify({
+        status: 'failed'
+      })
+    };
+  }
+  else if (room.sid === 'in-progress') {
+    return {
+      statusCode: 425,
+      body: JSON.stringify({
+        status: 'in-progress'
+      })
+    };
+  }
+  else {
+    // room status is completed
+      const response = await client.video.v1.compositions
+                                 .create({
+                                   audioSources: ['*'],
+                                   videoLayout: {
+                                     grid: {
+                                       video_sources: [
+                                         '*'
+                                       ]
+                                     }
+                                   },
+                                   statusCallback: statusCallback,
+                                   format: 'mp4',
+                                   roomSid: roomSid
+                                 })
+                                 .then(handleSuccess)
+                                 .catch(handleError);
+    return response;
+  }
+};
