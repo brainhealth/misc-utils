ea7b8ae8c5aac1a1bdbd42a84acedf5afc7ab865
= <=>
2023-09-29 15:48:44 -0500

    handy dandy way of breaking down url style params

--- twilio-receive-message-webhook/index.js
+++ twilio-receive-message-webhook/index.js
@@ -12,10 +12,18 @@ export const handler = async (event) => {
   const ServiceID = jsonData.MessagingServiceSid;
 
   const response = new MessagingResponse();
-  if (!event.body.includes("STOP")) {
+  const bodyDict = event.body.split("&").map(kv => kv.split("=")).reduce((m, kv) => {
+    m[kv[0]] = kv[1];
+    return m;
+  }, {"initkey" : "initVal"});
+
+  console.log("bodyDict", bodyDict);
+  console.log("message body", bodyDict['Body']);
+
+  if (bodyDict['Body'].toUpperCase().includes("YES") || bodyDict['Body'].toUpperCase().includes("BRAIN")) {
     response.message("You are now opted in to receive event communications from CBH! We're excited to have you on this journey with us! STOP at any time to stop receiving messages.");
   }
-  else {
+  else if (bodyDict['Body'].toUpperCase().includes("STOP")) {
     response.message("You are now opted out of receive event communications from CBH!");
   }
 
